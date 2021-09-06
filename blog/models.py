d200a09b2dab2d0e82b7a7a42e3541fd37bc0a86
roomman <roomman01@naver.com>
2021-09-06 22:56:36 +0900

    FBV로 블로그 포스트 상세페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
@@ -10,3 +11,7 @@ class Post(models.Model):
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
+    
--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
-    path('blog', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
