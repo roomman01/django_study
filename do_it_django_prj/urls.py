c884552c32a07e30d3a5e8ba8577c4e0d75ae334
roomman <roomman01@naver.com>
2021-10-17 10:51:09 +0900

    포스트 목록 페이지에 대표이미지 출력하기

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -15,11 +15,16 @@ Including another URLconf
 """
 from django import urls
 from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('', include('single_pages.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
