009925827131ed963885e727858d2447569c7cc7
roomman <roomman01@naver.com>
2021-10-26 23:36:37 +0900

    태그 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.shortcuts import render
-from .models import Post, Category
+from .models import Post, Category, Tag
 from django.views.generic import ListView, DetailView
 
 class PostList(ListView):
@@ -38,6 +38,21 @@ def category_page(request, slug):
             'categories' : Category.objects.all(),
             'no_category_post_count' : Post.objects.filter(category=None).count(),
             'category' : category,
+        }
+    )
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list' : post_list,
+            'tag' : tag,
+            'no_category_post_count' : Post.objects.filter(category=None).count(),
+            'categories' : Category.objects.all(),
         }
     )
 
